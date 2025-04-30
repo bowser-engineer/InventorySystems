8015d946bf1679e36bd887711ba38811ba835099
Stephen Ulibarri <druidmech@gmail.com>
2025-04-30 12:46:33 -0600

    Inventory Component

---
+++ Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/Inv_InventoryComponent.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Inv_InventoryComponent.generated.h"
+
+
+class UInv_InventoryBase;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
+class INVENTORY_API UInv_InventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UInv_InventoryComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+
+	TWeakObjectPtr<APlayerController> OwningController;
+	
+	void ConstructInventory();
+
+	UPROPERTY()
+	TObjectPtr<UInv_InventoryBase> InventoryMenu;
+
+	UPROPERTY(EditAnywhere, Category = "Inventory")
+	TSubclassOf<UInv_InventoryBase> InventoryMenuClass;
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
