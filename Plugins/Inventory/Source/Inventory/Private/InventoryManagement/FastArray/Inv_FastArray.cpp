8360f8f198c47f7ca3474db1e1768931d9f5bdc2
Stephen Ulibarri <druidmech@gmail.com>
2025-05-01 13:19:36 -0600

    Fast Array Serializer

---
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/FastArray/Inv_FastArray.cpp
@@ -0,0 +1,32 @@
+
+#include "InventoryManagement/FastArray/Inv_FastArray.h"
+
+TArray<UInv_InventoryItem*> FInv_InventoryFastArray::GetAllItems() const
+{
+	
+}
+
+void FInv_InventoryFastArray::PreReplicatedRemove(const TArrayView<int32> RemovedIndices, int32 FinalSize)
+{
+	
+}
+
+void FInv_InventoryFastArray::PostReplicatedAdd(const TArrayView<int32> AddedIndices, int32 FinalSize)
+{
+	
+}
+
+UInv_InventoryItem* FInv_InventoryFastArray::AddEntry(UInv_ItemComponent* ItemComponent)
+{
+	
+}
+
+UInv_InventoryItem* FInv_InventoryFastArray::AddEntry(UInv_InventoryItem* Item)
+{
+	
+}
+
+void FInv_InventoryFastArray::RemoveEntry(UInv_InventoryItem* Item)
+{
+	
+}
