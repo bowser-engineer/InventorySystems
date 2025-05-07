4758b8f16337cb0a737b5fa57e96b1c5d73a6594
Stephen Ulibarri <druidmech@gmail.com>
2025-05-07 16:14:15 -0600

    Exiting the Loop Early

--- Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
+++ Plugins/Inventory/Source/Inventory/Private/Widgets/Inventory/Spatial/Inv_InventoryGrid.cpp
@@ -50,7 +50,11 @@ FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const FInv_ItemMa
 	int32 AmountToFill = StackableFragment ? StackableFragment->GetStackCount() : 1;
 	
 	// For each Grid Slot:
+	for (const auto& GridSlot : GridSlots)
+	{
 		// If we don't have anymore to fill, break out of the loop early.
+		if (AmountToFill == 0) break;
+		
 		// Is this index claimed yet?
 		// Can the item fit here? (i.e. is it out of grid bounds?)
 		// Is there room at this index? (i.e. are there other items in the way?)
@@ -62,6 +66,8 @@ FInv_SlotAvailabilityResult UInv_InventoryGrid::HasRoomForItem(const FInv_ItemMa
 			// If stackable, is this slot at the max stack size already?
 		// How much to fill?
 		// Update the amount left to fill
+	}
+		
 	// How much is the Remainder?
 	
 	return Result;
