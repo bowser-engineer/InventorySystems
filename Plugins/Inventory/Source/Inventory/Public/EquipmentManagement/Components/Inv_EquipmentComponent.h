83920a1c7fa80cd8fff694f2bb12ea36e0515925
Stephen Ulibarri <druidmech@gmail.com>
2025-06-03 11:45:56 -0600

    Equipment Component

---
+++ Plugins/Inventory/Source/Inventory/Public/EquipmentManagement/Components/Inv_EquipmentComponent.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Inv_EquipmentComponent.generated.h"
+
+class UInv_InventoryComponent;
+class UInv_InventoryItem;
+class APlayerController;
+class USkeletalMeshComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
+class INVENTORY_API UInv_EquipmentComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+
+	TWeakObjectPtr<UInv_InventoryComponent> InventoryComponent;
+	TWeakObjectPtr<APlayerController> OwningPlayerController;
+	TWeakObjectPtr<USkeletalMeshComponent> OwningSkeletalMesh;
+
+	UFUNCTION()
+	void OnItemEquipped(UInv_InventoryItem* EquippedItem);
+
+	UFUNCTION()
+	void OnItemUnequipped(UInv_InventoryItem* EquippedItem);
+
+	void InitInventoryComponent();
+};
